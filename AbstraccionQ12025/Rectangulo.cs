45705b3c5aeccf6c9d6ebd61c71e1b23a8c19c8e
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-27 19:13:10 -0600

    Create Rectangle class

---
+++ AbstraccionQ12025/Rectangulo.cs
@@ -0,0 +1,20 @@
+﻿
+namespace AbstraccionQ12025
+{
+    public class Rectangulo : Figura
+    {
+        public double Base;
+
+        public double Altura;
+
+        public override double Area()
+        {
+            return Base * Altura;
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * (Base + Altura);
+        }
+    }
+}
