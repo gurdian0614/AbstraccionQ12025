6f78b63c6a63dd56b46f826699f2b4c84dd4f376
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-27 18:59:55 -0600

    Create abstract class

---
+++ AbstraccionQ12025/Figura.cs
@@ -0,0 +1,28 @@
+﻿
+namespace AbstraccionQ12025
+{
+    public abstract class Figura
+    {
+        /// <summary>
+        /// Abstract method that calculates the geometric area
+        /// </summary>
+        /// <returns>Area value</returns>
+        public abstract double Area();
+
+        /// <summary>
+        /// Abstract method that calculatrs the perimeter
+        /// </summary>
+        /// <returns>Perimeter Value</returns>
+        public abstract double Perimetro();
+
+        /// <summary>
+        /// Print the area and perimeter values
+        /// </summary>
+        /// <param name="NombreFigura">Geometric name</param>
+        public void Imprimir(string NombreFigura)
+        {
+            Console.WriteLine($"Area del {NombreFigura}: {Area()}");
+            Console.WriteLine($"Perímetro del {NombreFigura}: {Perimetro()}");
+        }
+    }
+}
