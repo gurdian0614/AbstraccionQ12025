4a1b352225bb3228d2c53590389591563f1d35e3
gurdian0614 <ivan.gurdian@outlook.com>
2025-01-27 19:11:05 -0600

    Create Circle class

---
+++ AbstraccionQ12025/Circulo.cs
@@ -0,0 +1,18 @@
+﻿
+namespace AbstraccionQ12025
+{
+    public class Circulo : Figura
+    {
+        public double Radio;
+
+        public override double Area()
+        {
+            return Math.PI * Math.Pow(Radio, 2);
+        }
+
+        public override double Perimetro()
+        {
+            return 2 * Math.PI * Radio;
+        }
+    }
+}
